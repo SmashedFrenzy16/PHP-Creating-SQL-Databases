3cb255dcc995cfa48c415b8a8b4993f084cff026
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-05-09 20:59:54 +0100

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -5,7 +5,14 @@ $username = "Martin";
 $password = "ILOVESQL";
 
 
+try {
+  
+  $con = new PDO("mysql:host=$servername", $username, $password);
+  $con->setAttribute(PDO::ATTER_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "CREATE DATABASE firstPDODatabase"
+  $con->exec($sql)
 
+}
 
 
 
